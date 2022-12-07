a6e58c28b32f490b13ebde29afc3e02cab9971da
GGG <GGG@DESKTOP-8N3GG4O>
2022-12-07 14:17:44 +0900

    커밋 메시지
    깃 연습을 위한 프로젝트를 생성 하였습니다.
    첫번째 클래스를 작성하였습니다.

---
+++ 221206 Q/src/Lotto.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Lotto {
+	public static void main(String[] args) {
+		Set<Integer> lotto = new TreeSet<>();
+		Random random = new Random();
+		
+		while (lotto.size() < 6) {
+			int number = random.nextInt(45) + 1;
+			lotto.add(number);
+		}
+		System.out.println("랜덤번호: "+ lotto);
+		
+		Set<Integer> lotto1 = new TreeSet<>();
+		lotto1.add(8);
+		lotto1.add(13);
+		lotto1.add(25);
+		lotto1.add(29);
+		lotto1.add(34);
+		lotto1.add(1);
+		System.out.println("추첨번호:"+ lotto1);
+		       
+		 
+		
+		
+		
+	}
+}
